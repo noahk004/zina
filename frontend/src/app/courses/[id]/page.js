05c99ffd6edf1128e7d73a1df83af5721e657dd2
Noah Kim <noahmk5002@gmail.com>
2024-10-19 22:11:22 -0700

    add deepgram

--- frontend/src/app/courses/[id]/page.js
+++ frontend/src/app/courses/[id]/page.js
@@ -16,7 +16,7 @@ export default function Page() {
         <div className="rounded-lg w-[1000px] bg-white px-7 py-7">
           <h1 className="text-3xl font-bold mb-4">Course {id}</h1>
 
-            <div>
+            <div className="mb-3">
                 <div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
@@ -24,7 +24,7 @@ export default function Page() {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
             </div>
-          <div className="flex gap-2 text-lg">
+          <div className="flex gap-2 text-lg justify-end">
             <Link
               href="/courses"
               className="bg-white rounded-lg px-4 py-1 border-2 border-purpleLight hover:bg-purpleLight duration-200"
