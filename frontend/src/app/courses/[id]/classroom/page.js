dd24f1820e032693471e275d703ff94462f383b0
Neet <75447256+Chhez@users.noreply.github.com>
2024-10-20 04:53:51 -0700

    Speech to Text Part 1

--- frontend/src/app/courses/[id]/classroom/page.js
+++ frontend/src/app/courses/[id]/classroom/page.js
@@ -1,18 +1,124 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { io } from 'socket.io-client';
+import {
+  LiveConnectionState,
+  LiveTranscriptionEvents,
+  useDeepgram,
+} from "../../../context/DeepgramContextProvider.js";
+import {
+  MicrophoneEvents,
+  MicrophoneState,
+  useMicrophone,
+} from "../context/MicrophoneContextProvider.js";
+import Visualizer from "./Visualizer.js";
 
 import Page from "./temppage";
 
 const socket = io("http://localhost:5050");
 
-function App() {
+function App() {  
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [index, setIndex] = useState(0);
   const [images, setImages] = useState([]);
   const [currAudioUrl, setCurrAudioUrl] = useState('');
+  const [caption, setCaption] = useState(
+    "Captions Look Like This"
+  );
+  const { connection, connectToDeepgram, connectionState } = useDeepgram();
+  const { setupMicrophone, microphone, startMicrophone, microphoneState } =
+    useMicrophone();
+  const captionTimeout = useRef();
+  const keepAliveInterval = useRef();
+
+  useEffect(() => {
+    setupMicrophone();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (microphoneState === MicrophoneState.Ready) {
+      connectToDeepgram({
+        model: "nova-2",
+        interim_results: true,
+        smart_format: true,
+        filler_words: true,
+        utterance_end_ms: 3000,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [microphoneState]);
+
+  useEffect(() => {
+    if (!microphone) return;
+    if (!connection) return;
+
+    const onData = (e) => {
+      // iOS SAFARI FIX:
+      // Prevent packetZero from being sent. If sent at size 0, the connection will close. 
+      if (e.data.size > 0) {
+        connection?.send(e.data);
+      }
+    };
+
+    const onTranscript = (data) => {
+      const { is_final: isFinal, speech_final: speechFinal } = data;
+      let thisCaption = data.channel.alternatives[0].transcript;
+
+      console.log("thisCaption", thisCaption);
+      if (thisCaption !== "") {
+        console.log('thisCaption !== ""', thisCaption);
+        setCaption(thisCaption);
+      }
+
+      if (isFinal && speechFinal) {
+        clearTimeout(captionTimeout.current);
+        captionTimeout.current = setTimeout(() => {
+          setCaption(undefined);
+          clearTimeout(captionTimeout.current);
+        }, 3000);
+      }
+    };
+
+    if (connectionState === LiveConnectionState.OPEN) {
+      connection.addListener(LiveTranscriptionEvents.Transcript, onTranscript);
+      microphone.addEventListener(MicrophoneEvents.DataAvailable, onData);
+
+      startMicrophone();
+    }
+
+    return () => {
+      // prettier-ignore
+      connection.removeListener(LiveTranscriptionEvents.Transcript, onTranscript);
+      microphone.removeEventListener(MicrophoneEvents.DataAvailable, onData);
+      clearTimeout(captionTimeout.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connectionState]);
+
+  useEffect(() => {
+    if (!connection) return;
+
+    if (
+      microphoneState !== MicrophoneState.Open &&
+      connectionState === LiveConnectionState.OPEN
+    ) {
+      connection.keepAlive();
+
+      keepAliveInterval.current = setInterval(() => {
+        connection.keepAlive();
+      }, 10000);
+    } else {
+      clearInterval(keepAliveInterval.current);
+    }
+
+    return () => {
+      clearInterval(keepAliveInterval.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [microphoneState, connectionState]);
   
   useEffect(() => {
     // Listen for incoming messages from the server
@@ -88,6 +194,13 @@ function App() {
         Send
       </button>
       {currAudioUrl && <audio src={currAudioUrl} controls autoPlay />}
+    </div>
+
+    <div className="relative w-full h-full">
+      {microphone && <Visualizer microphone={microphone} />}
+      <div className="absolute bottom-[8rem]  inset-x-0 max-w-4xl mx-auto text-center">
+        {caption && <span className="bg-black/70 p-8">{caption}</span>}
+      </div>
     </div>
     </>
   );
